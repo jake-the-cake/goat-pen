f65e5bf6d2e5651b63bf30c9ba98a5b6f6f7f637
Jakey Wakey <askjake331@gmail.com>
2023-02-28 19:33:24 -0500

    setup project

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+
+app.use(express.static('build'))
+
+const port = setPort()
+app.listen(port, function(){
+	console.log(`Server running on port ${port}`)
+})
+
+function setPort(port: number = 6047): string {
+	return process.env.PORT || String(port)
+}
