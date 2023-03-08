f5ac74008dce3a4d9bf4444372072f57d0161170
jake-the-cake <askjake331@gmail.com>
2023-03-08 15:26:14 -0500

    alert message

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ function App() {
     <div className="page">
       <Navbar />
       <main>
+        <div className="page-alert">
+          <span className="page-alert-title">Special Message</span>
+          <span className="page-alert-blob">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam nam laborum omnis, fuga consectetur adipisci facilis at doloremque unde vel, quam quis mollitia officia inventore fugiat. Vitae odio obcaecati tenetur?</span>
+        </div>
         <div className="content-container two-col-stack">
           <article className='flex-1'>
             <h2 className="content-header">Main Header</h2>
