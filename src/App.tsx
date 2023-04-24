747bc6eb7247236992955fd0b3e3ff3f4bad5d9b
jake-the-cake <askjake331@gmail.com>
2023-04-24 14:45:20 -0400

    new new layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,21 @@ function App() {
   console.log(window.location)
   return (
     <>
-      <div className="top-triangle"></div>
+      <div className="wrapper">
+        <header className="header__title">
+          <span className="header__title--text">
+            Quiet Goat Labs
+          </span>
+        </header>
+        <nav></nav>
+        <main>
+          <section></section>
+          <section></section>
+        </main>
+      </div>
+      {/* <div className="top-triangle"></div>
       <div className="left-triangle"></div>
-      <div className="top-bar"></div>
+      <div className="top-bar"></div> */}
     </>
   )
 }
