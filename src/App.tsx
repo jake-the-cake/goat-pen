8cc252451683450bd029e4b1a8d0ad49376a1fab
jake-the-cake <askjake331@gmail.com>
2023-05-04 13:49:32 -0400

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Header } from "./components/Header"
 import { Navbar } from "./components/Navbar"
 
 function App() {
   console.log(window.location)
   return (
-    <>
+    <BrowserRouter>
       <div className="wrapper">
         <Header />
         <Navbar />
-        <main className="main__container">
-          <section className="section__left">
-            left
-          </section>
-          <section className="section__right">
-            right
-          </section>
-        </main>
+          <Routes>
+            <Route path="/" element={
+              (
+                <main className="main__container">
+                  <section className="section__left">
+                    left
+                  </section>
+                  <section className="section__right">
+                    right
+                  </section>
+                </main>
+              )
+            } />
+          </Routes>
         <footer className="footer__container">
           <div className="footer__section">l</div>
           <div className="footer__section">r</div>
           <div className="footer__section">c</div>
         </footer>
       </div>
-    </>
+    </BrowserRouter>
   )
 }
 
