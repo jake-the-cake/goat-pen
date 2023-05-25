0fefd70cf71f198e56ca3c6fabb70171871001c6
jake-the-cake <askjake331@gmail.com>
2023-05-25 15:41:29 -0400

    refactor

--- backend/src/database/models/emails.ts
+++ backend/src/database/models/emails.ts
@@ -4,7 +4,8 @@ const emails = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Email address is required.']
-  }
-})
+  }},
+  { timestamps: true }
+)
 
 export const EmailsModel = mongoose.model('Emails', emails)
