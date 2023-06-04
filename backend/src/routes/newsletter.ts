e8fbc782044350c59375fa2a53db62dc3a4788e6
Jakey Wakey <askjake331@gmail.com>
2023-06-04 00:26:24 -0400

    it would not be nighttime without forgetting to save a file

--- backend/src/routes/newsletter.ts
+++ backend/src/routes/newsletter.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { validateEmail } from '../utils/validation'
 import { EmailsModel } from '../database/models/emails'
 import { all, insert } from '../database/controllers/generic'
+import { ReqType, ResType } from 'types/apiObjects'
 
 const router = express.Router()
 
@@ -13,7 +14,9 @@ router.route('/join')
   .post(insert(EmailsModel, { email: validateEmail }))
 
 router.route('/unsubscribe/:id' || '/unsubscibe')
-  .patch(() => console.log('patch'))
+  .patch(function(req: ReqType, res: ResType) {
+    console.log('patch')
+  })
 
 router.route('/contacts')
   .get(all(EmailsModel))
