99bb57645b5ae483ba22c04e375bab7234b25e34
jake-the-cake <askjake331@gmail.com>
2023-06-05 15:00:12 -0400

    building logger

--- backend/src/utils/logs.ts
+++ backend/src/utils/logs.ts
@@ -1,16 +1,50 @@
-import { CallbackIndex, StringIndex } from '../types/generic'
 import config from '../config'
 
 class ServerLog {
 	text: string
+	timestamp: Date
+	time?: string
+	date?: string
+	mode?: 'INFO' | 'ERROR' | 'WARN' | undefined
 
-	constructor(text: string) {
+	constructor(text: string = '') {
+		this.text = text
+		this.timestamp = new Date()
 		return this
+	}
+
+	private init() {
+		// this.date =
+	}
+
+	private decorate() {
+
+	}
+
+	getDate(): this {
+		const dateArray = this.timestamp.toLocaleDateString().split('/')
+
+		dateArray.forEach(function(item: string | number, i: number) {
+			if (String(item).split('').length === 1 && i !== 2) dateArray[i] = '0' + item
+		})
+
+		const [month, day, year] = dateArray
+		this.date = year + '.' + month + '.' + day
+		console.log(this.date)
+		return this
+	}
+
+	info(text: string = this.text) {
+
+	}
+
+	createLog(): void {
+		console.log(this.text)
 	}
 }
 
 class DevLog extends ServerLog {
-	devText?: string
+	isDev?: booleana
 
 	constructor(text: any) {
 		super(text)
@@ -19,12 +53,9 @@ class DevLog extends ServerLog {
 
 	checkMode(text: any): this | void {
 		if (config.mode !== 'DEV') return
-		this.text = this.devText = text
+		this.text = text
+		this.isDev = true
 		return this
-	}
-
-	createDevLog(): void {
-		console.log(this.text)
 	}
 }
 
@@ -34,8 +65,7 @@ export function serverLog(): void {
 
 export function devLog(text: any, mode?: string): void {
 	const log = new DevLog(text)
-	if (log.text) {
-		if (mode) (log)[mode]()
-		log.createDevLog()
-	}
+	if (log.isDev) log.createLog()
 }
+
+new ServerLog().getDate()
